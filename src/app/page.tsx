62e7aec535ef1c69a6fc692dedc3dfa719c5ac64
Oskari Kosonen <o.kosonen@gmail.com>
2023-07-08 23:49:31 +0300

    Pass current date as default for MiniCalendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MiniCalendar from '@/app/components/miniCalendar';
 export default function Home() {
   return (
     <div className="flex flex-wrap justify-center mt-3">
-      <MiniCalendar />
+      <MiniCalendar date={new Date()} />
       <div className="ml-4">
         <h2 className="text-xl">Current balance</h2>
         <div>+25,00h (mock for now)</div>
