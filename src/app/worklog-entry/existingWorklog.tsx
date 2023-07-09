34b7594381486123eb5b9692da64c10077657d4f
Oskari Kosonen <o.kosonen@gmail.com>
2023-07-09 18:02:04 +0300

    Add confirmation before deleting the worklog

--- src/app/worklog-entry/existingWorklog.tsx
+++ src/app/worklog-entry/existingWorklog.tsx
@@ -2,6 +2,12 @@ import { Worklog } from '@prisma/client';
 import { MdDelete, MdModeEdit } from 'react-icons/md';
 import WorklogTitle from './worklogTitle';
 
+function showConfirm(confirmId: string) {
+  (
+    window[confirmId as unknown as number] as unknown as { showModal(): void }
+  ).showModal();
+}
+
 export default function ExistingWorklog({
   worklog,
   onDelete,
@@ -9,22 +15,44 @@ export default function ExistingWorklog({
   worklog: Worklog;
   onDelete: (id: number) => void;
 }) {
+  const confirmId = `worklog-delete-confirm-${worklog.id}`;
   return (
     <div className="card w-full bg-base-200 shadow-xl mt-4">
-      <div className="card-body">
+      <div className="card-body pr-4">
         <div className="flex justify-between">
           <WorklogTitle worklog={worklog} />
           <div className="card-actions justify-end w-16">
-            <MdModeEdit />
-            <MdDelete
+            <MdModeEdit
+              className="w-6 h-6"
               onClick={() => {
-                onDelete(worklog.id);
+                console.log('xx');
               }}
+            />
+            <MdDelete
+              className="w-6 h-6"
+              onClick={() => showConfirm(confirmId)}
             />
           </div>
         </div>
         {worklog.comment ? <p>{worklog.comment}</p> : null}
       </div>
+      <dialog id={confirmId} className="modal modal-bottom sm:modal-middle">
+        <form method="dialog" className="modal-box">
+          <h3 className="font-bold text-lg">Confirmation</h3>
+          <p className="py-4">Are you sure you want to delete this worklog?</p>
+          <div className="modal-action">
+            <button className="btn">Cancel</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                onDelete(worklog.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </form>
+      </dialog>
     </div>
   );
 }
