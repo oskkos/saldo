cf9e08299b4e8c22a98037df444275091e996c54
Oskari Kosonen <o.kosonen@gmail.com>
2023-07-22 21:08:46 +0300

    Always use UTC time to
    This is to ensure that differences in timezones between
    server and the client wont affect on calculations

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,51 +1,56 @@
 import dayjs from 'dayjs';
 import isoWeek from 'dayjs/plugin/isoWeek';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import { Date_ISODay, Date_Time } from './dateFormatter';
 
 dayjs.extend(isoWeek);
 dayjs.extend(advancedFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('UTC');
 
 export function add(
   date: string | number | Date,
   value: number,
   unit: dayjs.ManipulateType,
 ) {
-  return dayjs(date).add(value, unit).toDate();
+  return dayjs.tz(date).add(value, unit).toDate();
 }
 
 export function daysInMonth(date: Date) {
-  return dayjs(date).daysInMonth();
+  return dayjs.tz(date).daysInMonth();
 }
 
 export function diffInMinutes(
   date1: string | number | Date,
   date2: string | number | Date,
 ) {
-  return dayjs(date1).diff(date2, 'm');
+  return dayjs.tz(date1).diff(date2, 'm');
 }
 
 export function endOfDay(date?: string | number | Date) {
-  return dayjs(date).endOf('day').toDate();
+  return dayjs.tz(date).endOf('day').toDate();
 }
 
 export function endOfMonth(date: Date) {
-  return dayjs(date).endOf('month').toDate();
+  return dayjs.tz(date).endOf('month').toDate();
 }
 
 export function sameDay(date1: Date, date2: Date) {
-  return dayjs(date1).isSame(dayjs(date2), 'day');
+  return dayjs.tz(date1).isSame(dayjs.tz(date2), 'day');
 }
 
 export function startOfDay(date?: string | number | Date) {
-  return dayjs(date).startOf('day').toDate();
+  return dayjs.tz(date).startOf('day').toDate();
 }
 
 export function startOfMonth(date?: string | number | Date) {
-  return dayjs(date).startOf('month').toDate();
+  return dayjs.tz(date).startOf('month').toDate();
 }
 export function now() {
-  return new Date();
+  return dayjs.tz().toDate();
 }
 
 export function subtract(
@@ -53,9 +58,9 @@ export function subtract(
   value: number,
   unit: dayjs.ManipulateType,
 ) {
-  return dayjs(date).subtract(value, unit).toDate();
+  return dayjs.tz(date).subtract(value, unit).toDate();
 }
 
 export function toDate(day: Date_ISODay, time: Date_Time) {
-  return dayjs(`${day} ${time}`).toDate();
+  return dayjs.tz(`${day} ${time}`).toDate();
 }
