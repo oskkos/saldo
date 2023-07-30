ea004e4ef24a51810dc9b33559fc6c1e191a4d06
Oskari Kosonen <o.kosonen@gmail.com>
2023-07-30 23:35:08 +0300

    Guard against missing month

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { startOfMonth } from '@/util/date';
 import MiniCalendar from '@/components/miniCalendar';
 import { getSession, getUserFromSession } from '@/auth/authSession';
 import { assertExists, assertIsYearAndMonth } from '@/util/assertionFunctions';
+import { Date_YearAndMonth } from '@/util/dateFormatter';
 
 export default async function Home({
   searchParams,
@@ -21,11 +22,15 @@ export default async function Home({
   const worklogs = await getWorklogs(user.id);
   const settings = await getSettings(user.id);
   assertExists(settings);
-  assertIsYearAndMonth(searchParams.month);
+  if (searchParams.month) {
+    assertIsYearAndMonth(searchParams.month);
+  }
   return (
     <div className="flex flex-wrap justify-center mt-4">
       <MiniCalendar
-        date={startOfMonth(searchParams.month || undefined)}
+        date={startOfMonth(
+          (searchParams.month as Date_YearAndMonth) || undefined,
+        )}
         beginDate={settings.begin_date}
         worklogs={worklogs}
       />
