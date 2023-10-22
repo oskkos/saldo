c90462639b181e6cd4e24b00c76962290646a90c
Oskari Kosonen <o.kosonen@gmail.com>
2023-10-22 16:21:58 +0300

    Nicer labels + zoom/pan support

--- src/app/statistics/workMinutesPerDayChart.tsx
+++ src/app/statistics/workMinutesPerDayChart.tsx
@@ -1,30 +1,57 @@
 'use client';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
 import { Line } from 'react-chartjs-2';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-);
+import { Chart } from 'chart.js/auto';
+import zoomPlugin from 'chartjs-plugin-zoom';
+import 'chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm';
+
+import { toDayMonthYear } from '@/util/dateFormatter';
+import { TooltipItem } from 'chart.js/auto';
+
+Chart.register(zoomPlugin);
 
 export const options = {
   responsive: true,
+  maintainAspectRatio: false,
+
   plugins: {
     legend: {
       position: 'bottom' as const,
+    },
+    tooltip: {
+      callbacks: {
+        title: function (x: TooltipItem<'line'>[]) {
+          return toDayMonthYear(new Date(x[0].parsed.x));
+        },
+      },
+    },
+    zoom: {
+      zoom: {
+        wheel: {
+          enabled: true,
+        },
+        pinch: {
+          enabled: true,
+        },
+        mode: 'xy' as const,
+      },
+      pan: {
+        enabled: true,
+        mode: 'xy' as const,
+      },
+    },
+  },
+  scales: {
+    y: {
+      suggestedMin: 0,
+    },
+    x: {
+      type: 'time' as const,
+      time: {
+        unit: 'day' as const,
+        displayFormats: {
+          day: 'D.M.YYYY',
+        },
+      },
     },
   },
 };
@@ -51,7 +78,7 @@ export default function WorkMinutesPerDayChart({
     ],
   };
   return (
-    <div className="pt-4 w-10/12">
+    <div className="pt-4 w-10/12 h-96">
       <Line options={options} data={data} />
     </div>
   );
