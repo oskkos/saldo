503e3d9d643c1fe20a2a623de7c815fb3346ca8c
Oskari Kosonen <o.kosonen@gmail.com>
2024-07-22 17:47:03 +0300

    perf: add monitoring and remove excessive prefetch
    Added manual Sentry spans around db operations,
    would be nice to have Prisma integration working though :-/
    In addition disabled prefetching for MiniCalendar

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -1,31 +1,47 @@
 import { AuthUser, SettingsData } from '@/types';
 import { prisma } from './prisma';
+import * as Sentry from '@sentry/nextjs';
 
 export async function upsertUser({ email, name }: AuthUser) {
-  const user = await prisma.user.upsert({
-    where: { email },
-    create: {
-      email,
-      name,
+  return await Sentry.startSpan(
+    { name: 'upsertUser', op: 'db.sql.prisma' },
+    async () => {
+      const user = await prisma.user.upsert({
+        where: { email },
+        create: {
+          email,
+          name,
+        },
+        update: { name },
+      });
+      return user;
     },
-    update: { name },
-  });
-  return user;
+  );
 }
 
 export async function getUser(email: string) {
-  const user = await prisma.user.findUnique({
-    where: { email },
-  });
-  return user;
+  return await Sentry.startSpan(
+    { name: 'getUser', op: 'db.sql.prisma' },
+    async () => {
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+      return user;
+    },
+  );
 }
 export async function getSettings(userId: number) {
-  const s = await prisma.settings.findUnique({
-    where: {
-      user_id: userId,
+  return await Sentry.startSpan(
+    { name: 'getSettings', op: 'db.sql.prisma' },
+    async () => {
+      const s = await prisma.settings.findUnique({
+        where: {
+          user_id: userId,
+        },
+      });
+      return s;
     },
-  });
-  return s;
+  );
 }
 export async function insertSettings(
   userId: number,
@@ -33,39 +49,49 @@ export async function insertSettings(
   initialBalanceHours: number,
   initialBalanceMins: number,
 ) {
-  const s = await prisma.settings.upsert({
-    where: {
-      user_id: userId,
+  return await Sentry.startSpan(
+    { name: 'insertSettings', op: 'db.sql.prisma' },
+    async () => {
+      const s = await prisma.settings.upsert({
+        where: {
+          user_id: userId,
+        },
+        create: {
+          user_id: userId,
+          begin_date: beginDate,
+          initial_balance_hours: initialBalanceHours,
+          initial_balance_mins: initialBalanceMins,
+        },
+        update: {},
+      });
+      return s;
     },
-    create: {
-      user_id: userId,
-      begin_date: beginDate,
-      initial_balance_hours: initialBalanceHours,
-      initial_balance_mins: initialBalanceMins,
-    },
-    update: {},
-  });
-  return s;
+  );
 }
 export async function upsertSettings(
   userId: number,
   { beginDate, initialBalanceHours, initialBalanceMins }: SettingsData,
 ) {
-  const s = await prisma.settings.upsert({
-    where: {
-      user_id: userId,
+  return await Sentry.startSpan(
+    { name: 'upsertSettings', op: 'db.sql.prisma' },
+    async () => {
+      const s = await prisma.settings.upsert({
+        where: {
+          user_id: userId,
+        },
+        create: {
+          user_id: userId,
+          begin_date: beginDate,
+          initial_balance_hours: initialBalanceHours,
+          initial_balance_mins: initialBalanceMins,
+        },
+        update: {
+          begin_date: beginDate,
+          initial_balance_hours: initialBalanceHours,
+          initial_balance_mins: initialBalanceMins,
+        },
+      });
+      return s;
     },
-    create: {
-      user_id: userId,
-      begin_date: beginDate,
-      initial_balance_hours: initialBalanceHours,
-      initial_balance_mins: initialBalanceMins,
-    },
-    update: {
-      begin_date: beginDate,
-      initial_balance_hours: initialBalanceHours,
-      initial_balance_mins: initialBalanceMins,
-    },
-  });
-  return s;
+  );
 }
