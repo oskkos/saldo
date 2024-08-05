fdfed5597a6b797bbc0dbb314f127c225e2b446d
Oskari Kosonen <o.kosonen@gmail.com>
2024-08-05 15:55:12 +0300

    feat: :sparkles: Implemented function for reset password email
    Integrated to Mailjet for sending password reset emails.

---
+++ src/services/forgotPasswordMailSender.ts
@@ -0,0 +1,38 @@
+import Mailjet from 'node-mailjet';
+import 'server-only';
+
+export function sendResetPasswordMail(recipient: string, token: string) {
+  const mailjet = Mailjet.apiConnect(
+    process.env.MAILJET_API_KEY as string,
+    process.env.MAILJET_SECRET_KEY as string,
+  );
+
+  const request = mailjet.post('send', { version: 'v3.1' }).request({
+    Messages: [
+      {
+        From: {
+          Email: 'saldo@k6.fi',
+          Name: 'saldo',
+        },
+        To: [
+          {
+            Email: recipient,
+          },
+        ],
+        TemplateID: Number(process.env.MAILJET_RESET_PASSWORD_TEMPLATE_ID),
+        TemplateLanguage: true,
+        Subject: 'Reset your saldo password',
+        Variables: {
+          resetpasswordlink: `${process.env.HOST}/reset-password?token=${token}`,
+        },
+      },
+    ],
+  });
+  request
+    .then((result) => {
+      console.log(result.body);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
