eb33e7ba5acff5d1b0421dc6e291558848afb9cf
Oskari Kosonen <o.kosonen@gmail.com>
2026-01-03 23:13:01 +0200

    chore: update dependencies and configure Prisma with PostgreSQL adapter
    - Updated Prisma dependencies to version 7.2.0 and added PostgreSQL adapter.
    - Created a new Prisma configuration file for datasource settings.
    - Modified Prisma schema to align with new configuration.
    - Updated global CSS imports for Tailwind and DaisyUI.
    - Refactored repository files to import Prisma models from the generated client.
    - Established a connection pool for PostgreSQL in the Prisma repository.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   ...(process.env.VERCEL ? {} : { distDir: 'build' }),
+
   images: {
     remotePatterns: [
       {
--- src/repository/prisma.ts
+++ src/repository/prisma.ts
@@ -1,12 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const pool = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+const adapter = new PrismaPg(pool);
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter: adapter,
     log: [/*'query', 'info', */ 'warn', 'error'],
   });
 
