d5c44c29dfa5fd7db034f235e6d91b4505ff1f67
Oskari Kosonen <o.kosonen@gmail.com>
2026-01-03 18:58:33 +0200

    chore: update configs
    - next.config.js to conditionally set distDir
    -  .next/dev/types/**/*.ts to tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build', // Avoid permission issues in .next
+  ...(process.env.VERCEL ? {} : { distDir: 'build' }),
   images: {
     remotePatterns: [
       {
